a444a0c873efc4df9aee6840b437efc758131694
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-02-02 01:59:20 +0100

    changed the app.js to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
 import React from "react";
+import {FcPlus} from 'react-icons/fc'
+import {IoIosSunny} from 'react-icons/io'
+import {FaTimes} from 'react-icons/fa'
 
 function App() {
   return (
    <>
     <div className="todo__body">
-      <h3>todo</h3>
+      <div className="todo__head">
+        <h3>todo</h3>
+        <IoIosSunny className="todo__head--icon"/>
+      </div>
      <form className="todo__form">
       <input className="todo__input" type="text" placeholder="Create a new todo..."/>
-      <button>Add</button>
+      <button><FcPlus/></button>
      </form>
      <ul className="todo__lists">
       <li className="todo__list">
-        <div>
-          <input className="todo__checkbox" type="checkbox" />
-          <p className="todo__text">Complete online Javascript course</p>
+        <div className="todo__item">
+          <input className="todo__checkbox" type="checkbox" id="checkbox"/>
+          <label className="todo__text" for="checkbox">Complete online Javascript course</label>
         </div>
+        <FaTimes className="todo__list--icon"/>
       </li>
      </ul>
      <div className="todo__actions">
@@ -24,7 +31,12 @@ function App() {
         <p>Active</p>
         <p>Completed</p>
       </div>
-      <p>Clear Completed</p>
+      <p className="todo__clear">Clear Completed</p>
+    </div>
+    <div className="todo__act--mobile">
+        <p>All</p>
+        <p>Active</p>
+        <p>Completed</p>
     </div>
     </div>
     
