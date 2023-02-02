0a6759ffa98edd666eba34279619fe19bd38d831
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-02-02 23:28:14 +0100

    passed the todo props

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import {FaTimes} from 'react-icons/fa'
 
-const Todo = () => {
+const Todo = ({todo}) => {
   return (
     <li className="todo__list">
         <div className="todo__item">
           <input className="todo__checkbox" type="checkbox" id="checkbox"/>
-          <label className="todo__text" for="checkbox">Complete online Javascript course</label>
+          <label className="todo__text" for="checkbox">{todo}</label>
         </div>
         <FaTimes className="todo__list--icon"/>
       </li>
