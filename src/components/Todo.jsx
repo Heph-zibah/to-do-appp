14a7b32ad42b40df20ea683eefa2e1fce2300379
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-02-02 23:06:07 +0100

    addec the todo list item to the component

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Todo = () => {
   return (
-    <div>Todo</div>
+    <li className="todo__list">
+        <div className="todo__item">
+          <input className="todo__checkbox" type="checkbox" id="checkbox"/>
+          <label className="todo__text" for="checkbox">Complete online Javascript course</label>
+        </div>
+        <FaTimes className="todo__list--icon"/>
+      </li>
   )
 }
 
