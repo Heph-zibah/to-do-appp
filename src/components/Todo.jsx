3292f1e0ee20a8d4de593b1f8aa6e3b739d28417
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-02-05 20:59:07 +0100

    added the props and events

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import {FaTimes} from 'react-icons/fa'
 
-const Todo = ({todo}) => {
+const Todo = ({todo, toggleComplete, deleteTodo}) => {
   return (
-    <li className="todo__list">
+    <li className={todo.completed ? 'todo__list--complete' :"todo__list"}>
         <div className="todo__item">
-          <input className="todo__checkbox" type="checkbox" id="checkbox"/>
-          <label className="todo__text" for="checkbox">{todo}</label>
+          <input onChange={() => toggleComplete(todo)} className="todo__checkbox" type="checkbox" id="checkbox" checked={todo.completed ? 'checked' : ''}/>
+          <label onClick={() => toggleComplete(todo)} className={todo.completed ? 'todo__text--complete' : "todo__text"} for="checkbox">{todo.text}</label>
         </div>
-        <FaTimes className="todo__list--icon"/>
+        <FaTimes onClick={() => deleteTodo(todo.id)} className="todo__list--icon"/>
       </li>
   )
 }
