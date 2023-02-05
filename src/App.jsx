37debdb37d21ff329de1dd93f749848ebd4e3add
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-02-05 21:00:05 +0100

    added the function ffor the crud

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,50 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {FcPlus} from 'react-icons/fc'
 import {IoIosSunny} from 'react-icons/io'
 import Todo from "./components/Todo";
+import {db} from './firebase';
+import {query,collection, onSnapshot, querySnapshot, updateDoc, doc, addDoc, deleteDoc} from 'firebase/firestore'
 
 function App() {
 
-const [todos, setTodos] = useState(['Complete online Javascript course', 'Jog round the campus', 'Read 1hour'])
+const [todos, setTodos] = useState([]);
+const [input, setInput] = useState('');
+// create todo
+const createTodo = async (e) => {
+  e.preventDefault()
+
+  if(input === '') {
+    alert('please enter a valid todo')
+    return
+  }
+  await addDoc(collection(db, 'todos'), {
+    text:input,
+    ccompleted: false
+  })
+  setInput('')
+}
+// read todo
+useEffect(() => {
+  const q = query(collection(db, 'todos'))
+  const unsubscribe = onSnapshot(q, (querySnapshot) => {
+    let todosArr = []
+    querySnapshot.forEach((doc) => {
+      todosArr.push({...doc.data(), id: doc.id})
+    })
+    setTodos(todosArr)
+  })
+  return unsubscribe
+}, [])
+// update todo
+const toggleComplete  = async (todo) => {
+  await updateDoc(doc(db, 'todos', todo.id), {
+    completed:!todo.completed
+  })
+}
+// delete todo
+const deleteTodo = async (id) => {
+  await deleteDoc(doc(db, 'todos', id))
+}
 
   return (
    <>
@@ -14,18 +53,19 @@ const [todos, setTodos] = useState(['Complete online Javascript course', 'Jog ro
         <h3>todo</h3>
         <IoIosSunny className="todo__head--icon"/>
       </div>
-     <form className="todo__form">
-      <input className="todo__input" type="text" placeholder="Create a new todo..."/>
+     <form onSubmit={createTodo} className="todo__form">
+      <input value={input} onChange={(e) => setInput(e.target.value)} className="todo__input" type="text" placeholder="Create a new todo..."/>
       <button><FcPlus/></button>
      </form>
      <ul className="todo__lists">
       {todos.map((todo, index)=> {
-       return <Todo key={index} todo={todo}/>
+       return <Todo key={index} todo={todo} toggleComplete={toggleComplete} deleteTodo={deleteTodo}/>
       })}
       
      </ul>
      <div className="todo__actions">
-      <p className="todo__count">5 items left</p>
+      {todos.length < 1 ? null : <p className="todo__count">{`${todos.length} items left`}</p>}
+      
       <div className="todo__act">
         <p>All</p>
         <p>Active</p>
