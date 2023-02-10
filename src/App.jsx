6203811216cda3fbad6defdd35689b458e0d1b07
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-02-10 10:30:24 +0100

    added the theme markup

--- src/App.jsx
+++ src/App.jsx
@@ -4,82 +4,75 @@ import {IoIosSunny} from 'react-icons/io'
 import Todo from "./components/Todo";
 import {db} from './firebase';
 import {query,collection, onSnapshot, querySnapshot, updateDoc, doc, addDoc, deleteDoc} from 'firebase/firestore'
+import CreateTodo from "./components/CreateTodo";
+import FilterSection from "./components/FilterSection";
+
+const FILTER_MAP = {
+  All: () => true,
+  Active: (toddo) => !toddo.completed,
+  Completed: (toddo) => toddo.completed
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
+
 
 function App() {
 
 const [todos, setTodos] = useState([]);
 const [input, setInput] = useState('');
-// create todo
-const createTodo = async (e) => {
-  e.preventDefault()
+const [filter, setFilter] = useState('All');
 
-  if(input === '') {
-    alert('please enter a valid todo')
-    return
-  }
-  await addDoc(collection(db, 'todos'), {
-    text:input,
-    ccompleted: false
-  })
-  setInput('')
-}
+// const todosNoun = todos.length !== 1 ? 'items' : 'item';
+
 // read todo
 useEffect(() => {
-  const q = query(collection(db, 'todos'))
+  const q = query(collection(db, 'todos'));
   const unsubscribe = onSnapshot(q, (querySnapshot) => {
-    let todosArr = []
+    let todosArr = [];
     querySnapshot.forEach((doc) => {
-      todosArr.push({...doc.data(), id: doc.id})
-    })
-    setTodos(todosArr)
-  })
-  return unsubscribe
-}, [])
+      todosArr.push({ ...doc.data(), id: doc.id });
+    });
+    setTodos(todosArr);
+  });
+  return () => unsubscribe();
+}, []);
+
 // update todo
-const toggleComplete  = async (todo) => {
+const toggleComplete = async (todo) => {
   await updateDoc(doc(db, 'todos', todo.id), {
-    completed:!todo.completed
-  })
-}
+    completed: !todo.completed,
+  });
+};
+
 // delete todo
 const deleteTodo = async (id) => {
-  await deleteDoc(doc(db, 'todos', id))
-}
+  await deleteDoc(doc(db, 'todos', id));
+};
+const [theme, setTheme] = useState('dark')
+
+const [filterStatus, setFilterStatus] = useState("");
+
+
+useEffect(() => {
+  document.body.className = theme
+}, [theme])
 
   return (
    <>
+    <div className={`wrapper ${theme}`}>
     <div className="todo__body">
-      <div className="todo__head">
-        <h3>todo</h3>
-        <IoIosSunny className="todo__head--icon"/>
-      </div>
-     <form onSubmit={createTodo} className="todo__form">
-      <input value={input} onChange={(e) => setInput(e.target.value)} className="todo__input" type="text" placeholder="Create a new todo..."/>
-      <button><FcPlus/></button>
-     </form>
+      <CreateTodo theme={theme} setTheme={setTheme}/>
      <ul className="todo__lists">
       {todos.map((todo, index)=> {
        return <Todo key={index} todo={todo} toggleComplete={toggleComplete} deleteTodo={deleteTodo}/>
       })}
-      
      </ul>
-     <div className="todo__actions">
-      {todos.length < 1 ? null : <p className="todo__count">{`${todos.length} items left`}</p>}
-      
-      <div className="todo__act">
-        <p>All</p>
-        <p>Active</p>
-        <p>Completed</p>
-      </div>
-      <p className="todo__clear">Clear Completed</p>
-    </div>
-    <div className="todo__act--mobile">
-        <p>All</p>
-        <p>Active</p>
-        <p>Completed</p>
-    </div>
+     
+     <FilterSection todos={todos} setTodos={setTodos} setFilterStatus={setFilterStatus}/>
     </div>
     
+    </div>
     </>
   );
 }
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -3,10 +3,10 @@ import {FaTimes} from 'react-icons/fa'
 
 const Todo = ({todo, toggleComplete, deleteTodo}) => {
   return (
-    <li className={todo.completed ? 'todo__list--complete' :"todo__list"}>
+    <li className={todo.completed ? 'todo__list--complete' : "todo__list"}>
         <div className="todo__item">
           <input onChange={() => toggleComplete(todo)} className="todo__checkbox" type="checkbox" id="checkbox" checked={todo.completed ? 'checked' : ''}/>
-          <label onClick={() => toggleComplete(todo)} className={todo.completed ? 'todo__text--complete' : "todo__text"} for="checkbox">{todo.text}</label>
+          <p onClick={() => toggleComplete(todo)} className={todo.completed ? 'todo__text--complete' : "todo__text"}>{todo.text}</ p>
         </div>
         <FaTimes onClick={() => deleteTodo(todo.id)} className="todo__list--icon"/>
       </li>
