02eab46d691b0864395df2c3f624aa6bc943011e
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-02-10 14:23:02 +0100

    revamp file

--- src/components/CreateTodo.jsx
+++ src/components/CreateTodo.jsx
@@ -1,25 +1,13 @@
-import React, {useEffect, useState} from 'react'
+import React, {useState} from 'react'
 import {FcPlus} from 'react-icons/fc'
 import {IoIosSunny} from 'react-icons/io'
 import {collection, addDoc} from 'firebase/firestore'
 import {db} from '../firebase';
+
+
 const CreateTodo = ({theme, setTheme}) => {
 
-  const [input, setInput] = useState('');
-  // const [theme, setTheme] = useState('dark')
-
-  // // set theme
-  // const toggleTheme = () => {
-  //   if (theme === 'dark') {
-  //     setTheme('light')
-  //   }else {
-  //     setTheme('dark')
-  //   }
-  // }
-
-  // useEffect(() => {
-  //   document.body.className = theme
-  // }, [theme])
+  // SET THEME
   const toggleTheme = () => {
     if (theme === 'dark') {
       setTheme('light')
@@ -27,21 +15,22 @@ const CreateTodo = ({theme, setTheme}) => {
       setTheme('dark')
     }
   }
+
   // create todo
+  const [input, setInput] = useState('');
+  const createTodo = async (e) => {
+    e.preventDefault()
 
-const createTodo = async (e) => {
-  e.preventDefault()
-
-  if(input === '') {
-    alert('please enter a valid todo')
-    return
+    if(input === '') {
+      alert('please enter a valid todo')
+      return
+    }
+    await addDoc(collection(db, 'todos'), {
+      text:input,
+      completed: false
+    })
+    setInput('')
   }
-  await addDoc(collection(db, 'todos'), {
-    text:input,
-    completed: false
-  })
-  setInput('')
-}
   return (
     <>
     <header className={`todo__head ${theme}`}>
