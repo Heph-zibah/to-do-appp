88bcdefc5ef426a57e67579ccf8003e1e24d7548
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-02-12 22:22:11 +0100

    changed the id, label, and htmlfor attribute

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -5,8 +5,8 @@ const Todo = ({todo, toggleComplete, deleteTodo}) => {
   return (
     <li className={todo.completed ? 'todo__list--complete' : "todo__list"}>
         <div className="todo__item">
-          <input onChange={() => toggleComplete(todo)} className="todo__checkbox" type="checkbox" id="checkbox" checked={todo.completed ? 'checked' : ''}/>
-          <p onClick={() => toggleComplete(todo)} className={todo.completed ? 'todo__text--complete' : "todo__text"}>{todo.text}</ p>
+          <input onChange={() => toggleComplete(todo)} className="todo__checkbox" type="checkbox" id={`checkbox-${todo.id}`} checked={todo.completed ? 'checked' : ''}/>
+          <label htmlFor={`checkbox-${todo.id}`} onClick={() => toggleComplete(todo)} className={todo.completed ? 'todo__text--complete' : "todo__text"}>{todo.text}</ label>
         </div>
         <FaTimes onClick={() => deleteTodo(todo.id)} className="todo__list--icon"/>
       </li>
