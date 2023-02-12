03683f2ff5bddb969f38b49a299bec5662a8daa2
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-02-12 22:24:02 +0100

    revamp file

--- src/components/FilterSection.jsx
+++ src/components/FilterSection.jsx
@@ -1,11 +1,9 @@
-import React, { useState } from 'react'
+import React from 'react'
 
-
-
-const FilterSection = ({todos, setTodos,setFilterStatus, filterList}) => {
+const FilterSection = ({todos, setTodos,setFilterStatus,  filterList}) => {
   const clearCompletedTodos = () => {
     setTodos(todos.filter((todo) => !todo.completed));
-    setFilterStatus("all");
+    setFilterStatus('');
   };
   const todosNoun = todos.length !== 1 ? 'items' : 'item';
 
@@ -14,7 +12,7 @@ const FilterSection = ({todos, setTodos,setFilterStatus, filterList}) => {
       <div className="todo__actions">
         {todos.length < 1 ? null : <p className="todo__count">{`${todos.length} ${todosNoun} left`}</p>}
         <div className='todo__act'>{filterList}</div>
-        <p className="todo__clear" onClick={() => clearCompletedTodos}>Clear Completed</p>
+        <p className="todo__clear" onClick={clearCompletedTodos}>Clear Completed</p>
       </div>
       <div className="todo__act--mobile">
       {filterList}
